d8d9e092ece05262ba6e8ce56d7bf55d43c983ba
Ahmed AbdElgwad <Ahmed AbdElgwad@example.com>
2025-08-30 18:32:05 +0300

    ..

--- middleware.js
+++ middleware.js
@@ -12,5 +12,6 @@ export async function middleware(request) {
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ['/dashboard/:path*',"/create-meeting-form"]
+  matcher: ['/dashboard/:path*',"/create-meeting-form"] ,
+  runtime: 'nodejs', 
 }
